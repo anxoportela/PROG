7663bb5a4674a1f955045fcd5949013735718885
Anxo Portela-Insua Blanco <dual108@A33-E20.acarballeira.local>
2019-10-11 21:28:39 +0200

    Update

---
+++ Java/Bienvenido_Anxo/src/bienvenido1_anxo/Bienvenido2_Anxo.java
@@ -0,0 +1,20 @@
+//Imprimir texto
+
+package bienvenido1_anxo;
+
+/**
+ *
+ * @author Anxo Portela-Insua Blanco
+ */
+public class Bienvenido2_Anxo {
+
+    /**
+     * @param args Argumentos de la linea de comandos.
+     */
+    public static void main(String[] args) {
+        // TODO aplicar la lógica del programa.
+        System.out.print("Bienvenido a la ");
+        System.out.println("programación en java!");
+    }// Fin del método main.
+    
+}// Fin de la clase Bienvenido2.
---
+++ Java/Bienvenido_Anxo/src/bienvenido1_anxo/Bienvenido4_Anxo.java
@@ -0,0 +1,20 @@
+//Imprimir texto
+
+package bienvenido1_anxo;
+
+/**
+ *
+ * @author Anxo Portela-Insua Blanco
+ */
+public class Bienvenido4_Anxo {
+
+    /**
+     * @param args Argumentos de la linea de comandos.
+     */
+    public static void main(String[] args) {
+        // TODO aplicar la lógica del programa.
+        System.out.printf("%s\n%s\n" ,
+        "Bienvenido a" , "la programación en Java!");
+    }// Fin del método main.
+    
+}// Fin de la clase Bienvenido4.
