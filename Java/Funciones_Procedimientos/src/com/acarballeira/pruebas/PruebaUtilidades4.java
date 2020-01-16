db8aa3ca60eebfcb855b932071015a06c13e5b13
Anxo Portela-insua Blanco <anxoportela@gmail.com>
2020-01-16 13:12:02 +0100

    Update

--- Java/Funciones_Procedimientos/src/com/acarballeira/pruebas/PruebaUtilidades4.java
+++ Java/Funciones_Procedimientos/src/com/acarballeira/pruebas/PruebaUtilidades4.java
@@ -8,8 +8,8 @@ package com.acarballeira.pruebas;
 
 /**
  *
- * @author Anxo Portela-Insua Blanco <anxoportela@gmail.com>
- * @versión 1.0
+ * @author Anxo Portela-Insua Blanco &#60; anxoportela@gmail.com &#62;
+ * @version 1.0
  *
  */
 
